970fa6e153afc52e34d339d925e835043bfc6942
frankkivLU <frankkiv@31-34-21.wireless.csail.mit.edu>
2016-03-30 16:18:51 -0400

    Face detection in Video/Image

--- VideoFaceTracking.py
+++ VideoFaceTracking.py
@@ -1,80 +1,60 @@
+import matplotlib.pyplot as plt
 import sys
 sys.path.append('/usr/local/lib/python2.7/site-packages')
-from collections import deque
-import numpy as np
-import argparse
-import imutils
 import cv2
- 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-    help="max buffer size")
-args = vars(ap.parse_args())
+import random 
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
-pts = deque(maxlen=args["buffer"])
- 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-    camera = cv2.VideoCapture(0)
-# otherwise, grab a reference to the video file
-#
+imagePath = 'TestPhoto.jpg'
+image = cv2.imread(imagePath)
 
 #load opencv training data
 faceCascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml') 
+#eyeCascade = cv2.CascadeClassifier('/usr/local/Cellar/opencv/2.4.12_2/share/OpenCV/haarcascades/haarcascade_eye.xml')
 
+#image2gray
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+font = cv2.FONT_HERSHEY_SIMPLEX
+faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8, minSize=(80, 80), flags = cv2.CASCADE_SCALE_IMAGE)
+#faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+#eyes = eyeCascade.detectMultiScale(gray)
+#print faces
+
+for (x, y, w, h) in faces:
+    
+    #add rect (sourceimage,lefttop, rightbottom, color, size)
+    cv2.rectangle(image, (x,y), (x+w,y+h), (14,201,255), 5)
+    #(video source, string, TopLeft, font, font-size, font-color, font-weight)
+    cv2.putText(image, str(random.randrange(20,30)), (x+(w/2)-30,y-10), font, 2, (14,201,255), 7)
+
+plt.imshow(cv2.cvtColor(image,cv2.COLOR_RGB2BGR))
+
+
+import imutils
+
+camera = cv2.VideoCapture(0)
 # keep looping
 while True:
     # grab the current frame
     (grabbed, frame) = camera.read()
  
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
-    if args.get("video") and not grabbed:
-        break
- 
     # resize the frame, blur it, and convert it to the HSV
     # color space
     frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
- 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-# find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
-    
-    
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8, minSize=(80, 80), flags = cv2.CASCADE_SCALE_IMAGE)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=8, minSize=(100, 100), flags = cv2.CASCADE_SCALE_IMAGE)
     for (x, y, w, h) in faces:
     
         #(sourceimage,lefttop, rightbottom, color, size)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (14,201,255), 5)
-        cv2.putText(frame, "Frank", (x+(w/2)-30,y-10), font, 2, (14,201,255), 7)
+        cv2.putText(frame, "Frank", (x,y-10), font, 2, (14,201,255), 7)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
  
     # if the 'q' key is pressed, stop the loop
-    if key == ord("q"):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
     
 # cleanup the camera and close any open windows
